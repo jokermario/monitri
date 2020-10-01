0f15f0df35f30c5d2ad68b954a20b0a5f70a3450
Nwachukwu Favour <pyragleinc@gmail.com>
2020-10-01 19:32:46 +0100

    all set for heroku

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,10 +6,11 @@ import (
 	"github.com/qiangxue/go-env"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 const (
-	defaultServerPort         = 8080
+	//defaultServerPort         = 8080
 	defaultAccessExpirationHours = 1
 	defaultRefreshExpirationHours = 72
 )
@@ -17,7 +18,7 @@ const (
 // Config represents an application configuration.
 type Config struct {
 	// the server port. Defaults to 8080
-	ServerPort int `yaml:"server_port" env:"SERVER_PORT"`
+	ServerPort string `yaml:"server_port" env:"SERVER_PORT"`
 	// the data source name (DSN) for connecting to the database. required.
 	DSN string `yaml:"dsn" env:"DSN,secret"`
 	//AES Encryption Key
@@ -58,8 +59,10 @@ func (c Config) Validate() error {
 // Load returns an application configuration which is populated from the given configuration file and environment variables.
 func Load(file string, logger log.Logger) (*Config, error) {
 	// default config
+
 	c := Config{
-		ServerPort:    defaultServerPort,
+		//ServerPort:    defaultServerPort,
+		ServerPort: os.Getenv("PORT"),
 		AccessTokenExpiration: defaultAccessExpirationHours,
 		RefreshTokenExpiration: defaultRefreshExpirationHours,
 	}

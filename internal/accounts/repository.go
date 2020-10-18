9da62990c3767b7ef2bb9f141e64e2b99a52330a
Nwachukwu Favour <pyragleinc@gmail.com>
2020-10-18 21:21:08 +0100

    added an endpoint that would used to check the users account verifications status

--- internal/accounts/repository.go
+++ internal/accounts/repository.go
@@ -10,14 +10,14 @@ import (
 )
 
 type Repository interface {
-	GetById(ctx context.Context, id string) (entity.Accounts, error)
-	GetIdEmailPhone(ctx context.Context, id string) (entity.Accounts, error)
+	GetAccountById(ctx context.Context, id string) (entity.Accounts, error)
+	GetAccountIdEmailPhone(ctx context.Context, id string) (entity.Accounts, error)
 	GetAccountByEmail(ctx context.Context, email string) (entity.Accounts, error)
 	GetAccountByPhone(ctx context.Context, phone string) (entity.Accounts, error)
-	Create(ctx context.Context, account entity.Accounts) error
-	Count(ctx context.Context) (int, error)
-	Update(ctx context.Context, accounts entity.Accounts) error
-	Delete(ctx context.Context, id string) error
+	AccountCreate(ctx context.Context, account entity.Accounts) error
+	AccountCount(ctx context.Context) (int, error)
+	AccountUpdate(ctx context.Context, accounts entity.Accounts) error
+	AccountDelete(ctx context.Context, id string) error
 	GetAccounts(ctx context.Context, offset, limit int) ([]entity.Accounts, error)
 	SetRedisKey(conn redis.Conn, exp int64, key, value string) error
 	GetRedisKey(conn redis.Conn, key string) (string, error)
@@ -27,6 +27,10 @@ type Repository interface {
 	CreateSettings(ctx context.Context, settings entity.Settings) error
 	UpdateSettings(ctx context.Context, settings entity.Settings) error
 	DeleteSettings(ctx context.Context, id string) error
+	TransactionCreate(ctx context.Context, transaction entity.Transactions) error
+	WalletCreate(ctx context.Context, wallet entity.Wallets) error
+	GetTransactionByTransRef(ctx context.Context, transRef string) (entity.Transactions, error)
+	TransactionUpdate(ctx context.Context, transaction entity.Transactions) error
 }
 
 type repository struct {
@@ -40,46 +44,58 @@ func NewRepository(db *dbcontext.DB, logger log.Logger) Repository {
 
 //------------------------------------------------------ACCOUNTS--------------------------------------------------------
 
-func (r repository) GetById(ctx context.Context, id string) (entity.Accounts, error) {
+func (r repository) GetAccountById(ctx context.Context, id string) (entity.Accounts, error) {
 	var account entity.Accounts
 	err := r.db.With(ctx).Select().Model(id, &account)
 	return account, err
 }
 
-func (r repository) GetIdEmailPhone(ctx context.Context, id string) (entity.Accounts, error) {
+func (r repository) GetAccountIdEmailPhone(ctx context.Context, id string) (entity.Accounts, error) {
 	var account entity.Accounts
-	err := r.db.With(ctx).Select("id", "email", "phone").From("accounts").Where(dbx.NewExp("id={:id}", dbx.Params{"id":id})).One(&account)
+	err := r.db.
+		With(ctx).
+		Select("id", "email", "phone").
+		From("accounts").
+		Where(dbx.NewExp("id={:id}", dbx.Params{"id":id})).One(&account)
 	return account, err
 }
 
 func (r repository) GetAccountByEmail(ctx context.Context, email string) (entity.Accounts, error) {
 	var account entity.Accounts
-	err := r.db.With(ctx).Select().From("accounts").Where(dbx.NewExp("email={:email}", dbx.Params{"email":email})).One(&account)
+	err := r.db.
+		With(ctx).
+		Select().
+		From("accounts").
+		Where(dbx.NewExp("email={:email}", dbx.Params{"email":email})).One(&account)
 	return account, err
 }
 
 func (r repository) GetAccountByPhone(ctx context.Context, phone string) (entity.Accounts, error) {
 	var account entity.Accounts
-	err := r.db.With(ctx).Select().From("accounts").Where(dbx.NewExp("phone={:phone}", dbx.Params{"phone":phone})).One(&account)
+	err := r.db.
+		With(ctx).
+		Select().
+		From("accounts").
+		Where(dbx.NewExp("phone={:phone}", dbx.Params{"phone":phone})).One(&account)
 	return account, err
 }
 
-func (r repository) Create(ctx context.Context, account entity.Accounts) error {
+func (r repository) AccountCreate(ctx context.Context, account entity.Accounts) error {
 	return r.db.With(ctx).Model(&account).Insert()
 }
 
-func (r repository) Count(ctx context.Context) (int, error) {
+func (r repository) AccountCount(ctx context.Context) (int, error) {
 	var count int
 	err := r.db.With(ctx).Select("COUNT(*)").From("accounts").Row(&count)
 	return count, err
 }
 
-func (r repository) Update(ctx context.Context, accounts entity.Accounts) error {
+func (r repository) AccountUpdate(ctx context.Context, accounts entity.Accounts) error {
 	return r.db.With(ctx).Model(&accounts).Update()
 }
 
-func (r repository) Delete(ctx context.Context, id string) error {
-	account, err := r.GetById(ctx, id)
+func (r repository) AccountDelete(ctx context.Context, id string) error {
+	account, err := r.GetAccountById(ctx, id)
 	if err != nil {
 		return err
 	}
@@ -92,11 +108,42 @@ func (r repository) GetAccounts(ctx context.Context, offset, limit int) ([]entit
 	return accounts, err
 }
 
+//-----------------------------------------------------TRANSACTIONS-----------------------------------------------------
+
+func (r repository) GetTransactionByTransRef(ctx context.Context, transRef string) (entity.Transactions, error) {
+	var transaction entity.Transactions
+	err := r.db.
+		With(ctx).
+		Select().
+		From("transactions").
+		Where(dbx.NewExp("transaction_id={:trans_ref}", dbx.Params{"trans_ref":transRef})).One(&transaction)
+	return transaction, err
+}
+
+func (r repository) TransactionCreate(ctx context.Context, transaction entity.Transactions) error {
+	return r.db.With(ctx).Model(&transaction).Insert()
+}
+
+func (r repository) TransactionUpdate(ctx context.Context, transaction entity.Transactions) error {
+	return r.db.With(ctx).Model(&transaction).Update()
+}
+
+//-------------------------------------------------------WALLETS--------------------------------------------------------
+
+func (r repository) WalletCreate(ctx context.Context, wallet entity.Wallets) error {
+	return r.db.With(ctx).Model(&wallet).Insert()
+}
+
+
 //------------------------------------------------------SETTINGS--------------------------------------------------------
 
 func (r repository) GetSettingsById(ctx context.Context, id string) (entity.Settings, error) {
 	var settings entity.Settings
-	err := r.db.With(ctx).Select().From("settings").Where(dbx.NewExp("account_id={:id}", dbx.Params{"id":id})).One(&settings)
+	err := r.db.
+		With(ctx).
+		Select().
+		From("settings").
+		Where(dbx.NewExp("account_id={:id}", dbx.Params{"id":id})).One(&settings)
 	return settings, err
 }
 
@@ -118,7 +165,8 @@ func (r repository) DeleteSettings(ctx context.Context, id string) error {
 
 //-----------------------------------------------------TRANSACTIONAL----------------------------------------------------
 
-func (r repository) updateAccountAndSettingsTableTrans(ctx context.Context, accounts entity.Accounts, settings entity.Settings, Type string) error {
+func (r repository) updateAccountAndSettingsTableTrans(ctx context.Context, accounts entity.Accounts,
+	settings entity.Settings, Type string) error {
 	if err := r.db.Transactional(ctx, func(ctx context.Context) error {
 		if Type == "insert" {
 			if accUpdateErr := r.db.With(ctx).Model(&accounts).Update(); accUpdateErr != nil {

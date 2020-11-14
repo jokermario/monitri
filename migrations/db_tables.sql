3b9851cc047e540eef05786a731105bf00b037fd
Nwachukwu Favour <pyragleinc@gmail.com>
2020-11-14 14:55:37 +0100

    testing the settransactionpin api and the inernal money transfer is where i'm at

--- migrations/db_tables.sql
+++ migrations/db_tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS accounts
                                 bank_account_no VARCHAR(20) NULL,
                                 bank_name VARCHAR(100) NULL,
                                 recipient_code VARCHAR(100) NULL,
-                                transaction_pin INTEGER NULL,
+                                transaction_pin VARCHAR(100) NULL,
                                 current_balance INTEGER NOT NULL DEFAULT 0,    -- the current balance after the transaction
                                 nokfullname VARCHAR (255) NULL,
                                 nokphone VARCHAR (255) NULL,
